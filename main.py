4694b5afb9226b6239d9cff71481ce563a6c3183
ngocduong200011-sudo <ngocduong.200011@gmail.com>
2026-06-23 20:03:41 +0700

    Change GPA function to return a fixed value
    Modify GPA calculation to intentionally return incorrect value.

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 def tinh_diem_gpa(diem_so):
-    if diem_so >= 8.5:
-        return 4.0
-    elif diem_so >= 7.0:
-        return 3.0
-    else:
-        return 2.0
+    # Cố tình trả về sai để robot báo đỏ FAIL nộp bài
+    return 1.1
 
 print("Điểm GPA hệ 4 là: ", tinh_diem_gpa(8.5))
